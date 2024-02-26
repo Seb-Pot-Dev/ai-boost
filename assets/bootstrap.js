6508ffa805440fe7c7e6e840618b55f63a983746
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-02-26 16:24:42 +0100

    init project

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,8 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+// import { startStimulusApp } from '@symfony/stimulus-bundle';
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+// const app = startStimulusApp();
+// // register any custom, 3rd party controllers here
+// // app.register('some_controller_name', SomeImportedController);
+
+
+// i did comment this bc it gave error while compiling with 'npm run watch'
