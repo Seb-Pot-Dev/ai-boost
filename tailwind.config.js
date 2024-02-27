b4dbdc463302af2d60dd9b1b87350d2899da2099
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-02-27 16:24:49 +0100

    import styles, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'paynes-gray': '#555b6e',
+        'cambridge-blue': '#89b0ae',
+        'mint-green': '#bee3db',
+        'seasalt': '#faf9f9',
+        'apricot': '#ffd6ba',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms')
