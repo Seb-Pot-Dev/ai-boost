5fbffaa55d59f62d548cb4b1b11b777bbe2a15f9
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-03-04 22:06:31 +0100

    migrations

---
+++ migrations/Version20240303200121.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240303200121 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chat ADD language VARCHAR(50) DEFAULT NULL, ADD character_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chat DROP language, DROP character_name');
+    }
+}
