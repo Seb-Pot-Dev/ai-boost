573d27a613514b9855c7bde6edf605b58dfc383e
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-03-05 19:33:52 +0100

    major front update, color changes, responsive++

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'mint-green': '#bee3db',
         'seasalt': '#faf9f9',
         'apricot': '#ffd6ba',
+
+        'magnolia': '#f4effaff',
+      'russian-violet': '#2f184bff',
+      'tekhelet': '#532b88ff',
+      'amethyst': '#9b72cfff',
+      'wisteria': '#c8b1e4ff',
+
+      'g-violet': '#d292ff',
+      'g-dark-blue': '#00002e',
+      'g-light-blue': '#3c46ff',
+      'g-light-blue-hovered': '#0000ff',
+      'g-dark-red':  '#170005',
       },
     },
   },
