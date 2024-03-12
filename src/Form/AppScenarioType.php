99a67ca57fd848df019fa6610baf8ba5dcd648c0
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-03-12 13:09:06 +0100

    added placeholder/options

--- src/Form/AppScenarioType.php
+++ src/Form/AppScenarioType.php
@@ -21,6 +21,9 @@ class AppScenarioType extends AbstractType
         $builder
             ->add('characterName', TextType::class, [
                 'label' => 'Nom de votre personnage',
+                'attr' => [
+                    'placeholder' => 'Ex: Lancelot, Harry Potter, ...',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Veuillez entrer un nom pour votre personnage.',
@@ -37,20 +40,24 @@ class AppScenarioType extends AbstractType
                     'Romance' => 'Romance',
                     'Aventure' => 'Adventure',
                     'Action' => 'Action',
+                    'Realiste' => 'Realistic',
                 ],
                 'expanded' => true, // pour rendre ce champ comme un groupe de checkboxes
                 'multiple' => true, // permet de sélectionner plusieurs options            
             ])
             ->add('authorName', TextType::class, [
                 'label' => "Indiquez un auteur pour le style d'écriture",
-                'required' => false
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Ex: J.K. Rowling, Stephen King, ...',
+                ],
             ])
             ->add('wordsCount', ChoiceType::class, [ // Utilise ChoiceType au lieu de IntegerType
                 'label' => 'Response length (in words)',
                 'choices' => [
-                    'short' => 40, // Le label "40 words" mappé à la valeur 40
-                    'medium' => 50, // Le label "50 words" mappé à la valeur 50
-                    'long' => 60, // Le label "60 words" mappé à la valeur 60
+                    'court' => 40,
+                    'moyen' => 50,
+                    'long' => 60,
                 ],
                 'expanded' => true, // false pour une liste déroulante, true pour des boutons radio
                 'multiple' => false, // false car un seul choix est possible
